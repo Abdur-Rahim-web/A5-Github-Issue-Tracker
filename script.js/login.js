e3da1b59efeb24a640b595d7a51ca8b73cbc2aff
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-03-08 03:26:08 +0600

    fixed loading issue in login.js file.

--- script.js/login.js
+++ script.js/login.js
@@ -9,7 +9,7 @@ const loginBtn = () =>{
     if(name==="admin" & pin==="admin123"){
         alert('Login successful');
         
-        window.location.assign("/home.html");
+        window.location.assign("home.html");
     }
     else{
         alert('Login Fail');
