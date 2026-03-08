c9610384110e17b78640955fcae46ed468ed8417
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-03-08 18:06:03 +0600

    added loadingSpinner & toggleButton in home.js

--- script.js/home.js
+++ script.js/home.js
@@ -1,11 +1,43 @@
 const allIssueContainer = document.getElementById('allIssue-container');
 const totalIssue = document.getElementById('total-issue');
+const loadingSpinner = document.getElementById('loadingSpinner');
 
+// for toggleButton
+const allBtn = document.getElementById('all-btn');
+const openBtn = document.getElementById('open-btn');
+const closedBtn = document.getElementById('closed-btn');
+
+function toggleButton(id){
+    allBtn.classList.remove('btn-primary','text-white');
+    openBtn.classList.remove('btn-primary','text-white');
+    closedBtn.classList.remove('btn-primary','text-white');
+
+    allBtn.classList.add('bg-white','text-black');
+    openBtn.classList.add('bg-white','text-black');
+    closedBtn.classList.add('bg-white','text-black');
+
+    const selected = document.getElementById(id); //jata click hoba sata dorba
+    // console.log(selected);
+
+    selected.classList.remove('bg-white','text-black');
+    selected.classList.add('btn-primary','text-white');
+}
+
+
+function showLoading(){
+    loadingSpinner.classList.remove('hidden');
+    allIssueContainer.innerHTML = '';
+}
+
+function hiddenLoading(){
+    loadingSpinner.classList.add('hidden');
+}
 
 async function loadAllIssues(){
+    showLoading();
     const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const data = await res.json();
-
+    hiddenLoading();
     displayAllIssue(data.data);
 }
 
